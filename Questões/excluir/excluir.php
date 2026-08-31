29e763cb078acfe20813a202f6b020238c7e0a8a
RobertoAlvesAntunes <alvesantunesroberto77@gmail.com>
2026-08-31 03:54:33 -0300

    Implement discipline deletion functionality
    This script handles the deletion of a discipline from a text file based on the provided 'sigla'. It reads from 'disciplinas.txt', writes to 'disciplinasNovo.txt', and displays a success message.

--- Questões/excluir/excluir.php
+++ Questões/excluir/excluir.php
@@ -1,1 +1,45 @@
+<?php
+    $sigla = "";
+    $msg = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')  {
+    $sigla = $_GET["sigla"];
+    $msg = "";
+
+    $arqDisc = fopen("disciplinas.txt","r") or die("erro ao abrir arquivo");
+    $arqDiscNovo = fopen("disciplinasNovo.txt","w") or die("erro ao criar arquivo");
+
+    $linha = fgets($arqDisc);
+    fwrite($arqDiscNovo,$linha);
+
+    while(!feof($arqDisc)) {
+        $linha = fgets($arqDisc);
+        $colunaDados = explode(";", $linha);
+
+        if ($colunaDados[1] != $sigla) {
+            fwrite($arqDiscNovo,$linha);
+        }
+    }
+
+    fclose($arqDisc);
+    fclose($arqDiscNovo);
+
+    $msg = "Deu tudo certo!!!";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<h1>Excluir Disciplina</h1>
+
+<p><?php echo $msg ?></p>
+
+<br>
+
+</body>
+</html>
 
